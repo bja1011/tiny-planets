0020f23e7987f2f30a5b11c00c81d8ac9ead1b31
Adam Kaczmarek <bja1011@gmail.com>
2019-02-28 23:45:12 +0100

    dof based distance to objects

---
+++ Assets/keepOffset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepOffset : MonoBehaviour
+{
+    public GameObject offsetTarget;
+    private Vector3 offsetValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetValue = offsetTarget.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = offsetTarget.transform.position - offsetValue;
+    }
+}
---
+++ Assets/lookingAt.cs
@@ -0,0 +1,39 @@
+﻿// Prints the name of the object camera is directly looking at
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class lookingAt : MonoBehaviour
+{
+    DepthOfField dofLayer = null;
+    Camera cam;
+    public GameObject pPVolGE;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+       
+
+    }
+
+    void Update()
+    {
+
+        PostProcessVolume volume = pPVolGE.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out dofLayer);
+
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.distance);
+            dofLayer.focusDistance.value = hit.distance;
+            //hit.transform.localScale = new Vector3(2f,2f,2f);  
+            print("I'm looking at " + hit.transform.name);
+        }
+           
+        else
+            print("I'm looking at nothing!");
+    }
+}
